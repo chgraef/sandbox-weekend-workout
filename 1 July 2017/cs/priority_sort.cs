5a500d0920ec0ee8454e34a73d3180abc991626f
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-07-01 21:55:21 +0300

    Create priority_sort.cs

---
+++ 1 July 2017/cs/priority_sort.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CshConsoleApplication7
+{
+    class Program
+    {
+        private static IEnumerable<TItem> PrioritySort<TItem>(
+            IEnumerable<TItem> values, TItem[] priorities
+        )
+        {
+            var lowestPriority = priorities.Length;
+            var priorityLookup = new Dictionary<TItem, Int32>();
+            
+            for (var priority = 0; priority < priorities.Length; ++priority) {
+                var item = priorities[priority];
+                priorityLookup.Add(item, priority);
+            }
+            
+            return 
+                values.OrderBy((item) => {
+                    if (priorityLookup.ContainsKey(item)) {
+                        return priorityLookup[item];
+                    } else {
+                        return lowestPriority;
+                    }
+                }).ThenBy((x) => x).AsEnumerable();
+        }
+
+        static void Main(string[] args)
+        {
+            var priorities   = new String[] { "lettuce", "apple" };
+            var greenBasket  = new String[] { "orange", "banana", "tomato", 
+                                              "passionfruit", "mango", "mango" };
+
+            var sortedBasket = PrioritySort(greenBasket, priorities);
+
+            Console.WriteLine(String.Join(" ", sortedBasket));
+        }
+    }
+}
