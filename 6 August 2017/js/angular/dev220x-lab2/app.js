04c6cdcc53853756b2c45a14280d50c3c1a70286
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-08-06 18:17:02 +0300

    Create app.js

---
+++ 6 August 2017/js/angular/dev220x-lab2/app.js
@@ -0,0 +1,30 @@
+
+let app = angular.module('my-app', []);
+
+app.controller('menu-controller', [
+  '$scope',
+  ($scope) => {
+    class Selection {
+      select(item) {
+        this.selection = item;
+      }
+      
+      toString() {
+        return this.selection || 'Your Selection';
+      }
+    }
+    
+    $scope.selection = new Selection();
+    
+    $scope.select = (item) => {
+      $scope.selection.select(item);
+    }
+    
+    $scope.$watch('selection.selection', 
+      (newValue, oldValue) => {
+        if (newValue) {
+          alert("You have selected " + newValue);
+        }
+      });
+  }
+]);
