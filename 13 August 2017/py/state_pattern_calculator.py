ffc6a8d706b2797db8479a9eac5c8385b9625f64
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-08-13 21:19:34 +0300

    Create state_pattern_calculator.py

---
+++ 13 August 2017/py/state_pattern_calculator.py
@@ -0,0 +1,68 @@
+class StateStack:
+    def __init__(self, current_state):
+        self._states = [current_state]
+        self._index  = 0
+
+    def append_state(self, state):
+        if self._index + 1 >= len(self._states):
+            self._states.append(state)
+        else:
+            self._states[self._index + 1] = state
+        self._index += 1
+
+    def current_state(self):
+        return self._states[self._index]
+
+    def undo(self, times=1):
+        undone_index = max(self._index - times, 0)
+        undone_state = self._states[undone_index]
+        self._index  = undone_index
+
+        return undone_state
+
+class CalculatorState:
+    def __init__(self, value):
+        self._value = value
+
+    def value(self):
+        return self._value
+
+class Calculator:
+    def __init__(self, value=0):
+        current_state = CalculatorState(value)
+        self._states  = StateStack(current_state)
+
+    def add(self, value):
+        current_value = self.value()
+        result        = current_value + value
+        new_state     = CalculatorState(result)
+        
+        self._states.append_state(new_state)
+
+    def value(self):
+        current_state = self._states.current_state()
+        current_value = current_state.value()
+        
+        return current_value
+
+    def undo(self, times=1):
+        self._states.undo(times)
+
+    def __str__(self):
+        value = self.value()
+        return str(value)
+
+calculator = Calculator()
+
+while True:
+    print('>>>', end=' ')
+    user_input = input()
+
+    if user_input.isdigit():
+        value = int(user_input)
+        calculator.add(value)
+    elif user_input.startswith('u'):
+        calculator.undo()
+
+    print(calculator)
+
