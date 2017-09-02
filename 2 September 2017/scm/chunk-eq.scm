5c5b857a84134fe1adcdb33cf5574e963dd693bf
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-03 02:06:49 +0300

    Fix code formatting

--- 2 September 2017/scm/chunk-eq.scm
+++ 2 September 2017/scm/chunk-eq.scm
@@ -4,8 +4,7 @@
             (rest (chunk-eq (cdr lst))))
            (cond [(null? rest)      (list (list x))]
                  [(= x (caar rest)) (cons (cons x (car rest)) (cdr rest))]
-                 [else              (cons (list x) rest)]
-               ))))
+                 [else              (cons (list x) rest)]))))
 
 (define values `(0 1 1 2 3 3 3 0))
 (chunk-eq values)
