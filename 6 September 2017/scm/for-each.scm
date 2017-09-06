bedbbfd3243709e5556d3e9a5f7bd089add62237
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-07 01:06:52 +0300

    Bug fix

--- 6 September 2017/scm/for-each.scm
+++ 6 September 2017/scm/for-each.scm
@@ -1,11 +1,16 @@
+(define (my-mod x y)
+  (if (> y x) x
+      (my-mod (- x y) y)))
+
 (define (rly-even? x)
-  (zero? (mod x 2)))
+  (zero? (my-mod x 2)))
 
 (define range (iota 10))
 
 (for-each print
   (map (lambda (pair)
-         (let ((x (cadr pair)))
-              (if x (list x "is even")
+         (let ((x (cadr pair))
+               (is-even (car pair)))
+              (if is-even (list x "is even")
                     (list x "is odd"))))
        (map list (map rly-even? range) range)))
