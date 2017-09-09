19df734f0e01c2bb9c145db52916a8c36baf8ab4
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-09 22:57:09 +0300

    Add examples

--- 9 September 2017/scm/longest-common-prefix.scm
+++ 9 September 2017/scm/longest-common-prefix.scm
@@ -6,3 +6,8 @@
            (if (eq? x y)
                (cons x (longest-common-prefix (cdr lst1) (cdr lst2)))
                `()))))
+
+(for-each print
+          (list (longest-common-prefix `(a b c) `(a b d f))
+                (longest-common-prefix `(s t e r n) `(s t e r n u m))
+                (longest-common-prefix `(1 2 3) `(0 1 2 3))))
