da4587f367f6cb5938aeba40d2b1b85d3c5668e9
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-10 22:21:36 +0300

    Improve code formatting

--- 10 September 2017/scm/list-flatten.scm
+++ 10 September 2017/scm/list-flatten.scm
@@ -4,7 +4,7 @@
            (let ((y (if (list? x)
                         (list-flatten x)
                         (list x))))
-           (append y (list-flatten (cdr lst)))))))
+                (append y (list-flatten (cdr lst)))))))
 
 (define lst `(1 2 ((3)) ((4) 5)))
 (list-flatten lst)
