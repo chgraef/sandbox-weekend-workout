3dbe0880dc9eb303a2ec7f3c54bbe98a6e9ee654
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-11 21:44:00 +0300

    No longer leaking values from iota to the repeat fn

--- 11 September 2017/scm/left-justify.scm
+++ 11 September 2017/scm/left-justify.scm
@@ -1,14 +1,13 @@
 (define (repeat fn n)
-  (map fn (iota n)))
+  (map (lambda (_x) (fn)) (iota n)))
 
-(define (left-justify str n)
-  (string-concat (cons str (repeat (lambda (x) " ")
+(define (right-justify str n)
+  (string-concat (cons str (repeat (lambda () " ")
                                    (- n (string-length str))))))
 
 (let ((message "air-speed of unladen swallow")
       (width   32))
-     (let ((full-text (left-justify message width)))
+     (let ((full-text (right-justify message width)))
           (display full-text)
           (display "!!!")
           (newline)))
-
