74a6434fb9a15d4bfd87bddc1d1e570bf9696c21
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-11 21:44:57 +0300

    Fix erroneous naming of the left-justify function

--- 11 September 2017/scm/left-justify.scm
+++ 11 September 2017/scm/left-justify.scm
@@ -1,13 +1,13 @@
 (define (repeat fn n)
   (map (lambda (_x) (fn)) (iota n)))
 
-(define (right-justify str n)
+(define (left-justify str n)
   (string-concat (cons str (repeat (lambda () " ")
                                    (- n (string-length str))))))
 
 (let ((message "air-speed of unladen swallow")
       (width   32))
-     (let ((full-text (right-justify message width)))
+     (let ((full-text (left-justify message width)))
           (display full-text)
           (display "!!!")
           (newline)))
