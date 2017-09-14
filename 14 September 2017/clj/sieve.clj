acbeaf34796b5d67961d3257856b58be99c3c6fe
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-09-14 20:21:58 +0300

    Create sieve.clj

---
+++ 14 September 2017/clj/sieve.clj
@@ -0,0 +1,16 @@
+(defn generate-sieve
+  ([n] (generate-sieve
+         (range 2 n) (int (Math/sqrt n))))
+  ([sieve x]
+          ;(println x)
+          ;(println sieve)
+          ;(newline)
+          (if (< x 2) sieve
+              (generate-sieve
+                (filter #(or (> (mod % x) 0)
+                             (= % x))
+                        sieve) (dec x)))))
+
+(let [x 102]
+     (generate-sieve x))
+
