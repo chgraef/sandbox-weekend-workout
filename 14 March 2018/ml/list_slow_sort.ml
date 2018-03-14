274e6c02cbf1c3ba77abf941221edaca8d6d7894
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2018-03-14 08:30:27 +0200

    Create list_slow_sort.ml

---
+++ 14 March 2018/ml/list_slow_sort.ml
@@ -0,0 +1,15 @@
+let rec list_insert_sorted lst x =
+    match lst with
+        | []       -> [x]
+        | hd :: tl -> if x <= hd then
+                        x :: hd :: tl
+                    else
+                        hd :: list_insert_sorted tl x;;
+
+let list_sort_slowly lst =
+    let rec list_sort_slowly_helper res lst =
+        (match lst with
+             | []     -> res
+             | hd::tl -> let new_res = list_insert_sorted res hd in
+                         list_sort_slowly_helper new_res tl);
+    in list_sort_slowly_helper [] lst;;
